3a8455e26c5b7acbcd0f8b02660c0e00fe1ca357
Niko <137782638+thesleepyniko@users.noreply.github.com>
2025-10-16 22:18:53 -0400

    Build working prototype of OTP authentication
    Co-authored-by: Taggie <taggie@yip.cat>

--- api/auth/main.py
+++ api/auth/main.py
@@ -1,45 +1,152 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
 from fastapi.responses import RedirectResponse
 from fastapi.exceptions import HTTPException
+import os
+import secrets
+from email.message import EmailMessage
+from pydantic import BaseModel
+import asyncio
+import redis.asyncio as redis
+import aiosmtplib
+import dotenv
+import jwt
+from datetime import datetime, timezone, timedelta
+
+dotenv.load_dotenv()
+
+r = redis.Redis()
+
+class OtpClientRequest(BaseModel):
+    email: str
+
+class OtpClientResponse(BaseModel):
+    email: str
+    otp: int
 
 router = APIRouter()
 
+# @router.route("/callback")
+# async def callback(request: Request):
+#     try:
+#         await client.handleSignInCallback(str(request.url)) # Handle a lot of stuff
+#         return RedirectResponse("/") # Redirect the user to the home page after a successful sign-in
+#     except Exception as e:
+#         # Change this to your error handling logic
+#         raise HTTPException(500)
+
+# @router.route("/sign-in")
+# async def sign_in(request: Request):
+#     # Get the sign-in URL and redirect the user to it
+#     return RedirectResponse(await client.signIn(
+#         redirectUri="http://localhost:8000/callback",
+#     ))
+    
+# @router.route("/sign-up")
+# async def sign_up(request: Request):
+#     # Get the sign-in URL and redirect the user to it
+#     return RedirectResponse(await client.signIn(
+#         redirectUri="http://localhost:8000/callback",
+#         interactionMode="signUp", # Show the sign-up page on the first screen
+#     ))
+
+# @router.route("/sign-out")
+# async def sign_out(request: Request):
+#     return RedirectResponse(
+#         # Redirect the user to the home page after a successful sign-out
+#         await client.signOut(postLogoutRedirectUri="http://localhost:8000/")
+#     )
+
 #@decorator
 def require_auth(func):
-    async def wrapper(*args, **kwargs):
-        if not is_user_authenticated():
+    async def wrapper(*args, request: Request, **kwargs):
+        if not await is_user_authenticated(request):
             return RedirectResponse("/login", status_code=401)
-        return await func(*args, **kwargs)
+        return await func(request, *args, **kwargs)
     return wrapper
 
 #@decorator
 def require_admin(func):
-    async def wrapper(*args, **kwargs):
-        if not is_user_authenticated() or not is_user_admin():
-            return RedirectResponse("/login", status_code=401)
-        elif is_user_authenticated and not is_user_admin():
+    async def wrapper(*args, request: Request, **kwargs):
+        if not await is_user_authenticated(request) or not await is_user_admin():
+            return RedirectResponse("/login", status_code=418)
+        elif await is_user_authenticated(request) and not await is_user_admin():
             return RedirectResponse("/home", status_code=403)
-        return await func(*args, **kwargs)
+        return await func(request, *args, **kwargs)
     return wrapper
 
 #@decorator
 def require_reviewer(func):
-    async def wrapper(*args, **kwargs):
-        if not is_user_authenticated() or not is_user_reviewer():
-            return RedirectResponse("/login", status_code=401)
-        elif is_user_authenticated and not is_user_reviewer():
+    async def wrapper(*args, request: Request, **kwargs):
+        if not await is_user_authenticated(request) or not await is_user_reviewer():
+            return RedirectResponse("/login", status_code=418)
+        elif await is_user_authenticated(request) and not await is_user_reviewer():
             return RedirectResponse("/home", status_code=403)
-        return await func(*args, **kwargs)
+        return await func(request, *args, **kwargs)
     return wrapper
 
 async def is_user_admin() -> bool: ...
 
 async def is_user_reviewer() -> bool: ...
 
-async def is_user_authenticated() -> bool: ... # validate_token()
+async def is_user_authenticated(request: Request) -> None:
+    token = request.cookies.get("sessionId")
+    if token is None:
+        raise HTTPException(status_code=401)
+    try:
+        if not os.getenv("JWT_SECRET"):
+            raise HTTPException(500)
+        decoded = jwt.decode(token, os.getenv("JWT_SECRET", ""), ["HS256"])
+        if datetime.now(timezone.utc) - timedelta(days=7) > datetime.fromtimestamp(decoded["iat"], timezone.utc):
+            raise HTTPException(status_code=401)
+    # TODO: add email verification implementation once postgres is set up
+    except Exception:
+        raise HTTPException(status_code=401)
+     # validate_token(), check cookies, not Authorization: Bearer xyz
+    # return True
 
 async def refresh_token(): ...
 
-async def send_otp(): ...
+@router.post("/api/send_otp")
+async def send_otp(request: Request, otp_request: OtpClientRequest): 
+    otp = secrets.SystemRandom().randrange(100000, 999999)
+    await r.setex(f"otp-{otp_request.email}", 300, otp)
+    message = EmailMessage()
+    message["From"] = os.getenv("SMTP_EMAIL", "example@example.com")
+    message["To"] = otp_request.email
+    message["Subject"] = "Aces OTP code"
+    message.set_content(f"Your OTP for Aces is {otp}! This code will expire in 5 minutes. \n Happy Hacking!\nAces Organizing Team")
 
-async def validate_otp(): ...
+    await aiosmtplib.send(
+        message, 
+        hostname=os.getenv("SMTP_SERVER", "smtp.example.com"), 
+        port=465,
+        username=message["From"],
+        password=os.getenv("SMTP_PWD", ""),
+        use_tls=True)
+    return {"success": True}
+
+@router.post("/api/validate_otp")
+async def validate_otp(request: Request, otp_client_response: OtpClientResponse, response: Response): 
+    if not os.getenv("JWT_SECRET"):
+        raise HTTPException(status_code=500)
+    stored_otp = await r.get(f"otp-{otp_client_response.email}")
+    if not stored_otp:
+        raise HTTPException(status_code=401, detail="Invalid OTP")
+    print(stored_otp)
+    if not str(stored_otp.decode("utf-8")).isnumeric(): 
+        raise HTTPException(status_code=500)
+    if not int(stored_otp) == otp_client_response.otp:
+        raise HTTPException(status_code=401, detail="Invalid OTP")
+    
+    await r.delete(f"otp-{otp_client_response.email}")
+    token = await generate_session_id(otp_client_response.email)
+    response.set_cookie(key="sessionId", 
+                        value=token, 
+                        httponly=True, 
+                        secure=True,
+                        max_age=604800)
+    return {"success": True}
+
+async def generate_session_id(email: str) -> str: 
+    token = jwt.encode({"sub": email, "iat": int(datetime.now(timezone.utc).timestamp())}, os.getenv("JWT_SECRET", ""), algorithm="HS256")
+    return token
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from fastapi import FastAPI
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import FileResponse, RedirectResponse
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Request, Form, Depends
+from fastapi.responses import FileResponse, RedirectResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 # from pyairtable import Api
 # from pyairtable.formulas import match
@@ -8,17 +9,45 @@ from slowapi import Limiter
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
+# from api.auth import client
+from api.auth import require_auth, is_user_authenticated
+from api.auth import router as auth_router
 import orjson
 import os
 import dotenv
 import asyncpg
-from api.users import foo
+# from api.users import foo
 
 
 dotenv.load_dotenv()
 
 app = FastAPI()
+app.include_router(auth_router)
 
-@app.get("/test")
-async def test():
-    return foo()
+# @app.get("/test")
+# async def test():
+#     return foo()
+
+@app.get("/")
+async def home(request: Request):
+    # if client.is
+    # if client.isAuthenticated() is False:
+    #     return HTMLResponse("Not authenticated <a href='/sign-in'>Sign in</a>")
+
+    # return HTMLResponse("Authenticated <a href='/sign-out'>Sign out</a>")
+    return FileResponse("static/login.html")
+
+# @require_auth
+@app.get("/protectedroute")
+async def protected_route(request: Request, depends: str = Depends(is_user_authenticated)):
+    return HTMLResponse("<h1>Hello World! This is authenticated!</h1>")
+
+@app.get("/login")
+async def serve_login(request: Request):
+    pass
+
+# @app.post("/login")
+# async def handle_login(email: Annotated[str, Form()], otp: Annotated[int, Form()]):
+#     pass
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
